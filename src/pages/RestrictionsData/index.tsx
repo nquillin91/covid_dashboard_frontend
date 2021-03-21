1a186a38e4e8cec613190d5c516620360a726c2a
Shatilla Prayer <shatillaprayer@gmail.com>
2021-03-21 03:37:25 -0700

    Added the covid restrictions mock data

--- src/pages/RestrictionsData/index.tsx
+++ src/pages/RestrictionsData/index.tsx
@@ -1,3 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment from 'moment';
+
+
 export default function RestrictionsData() {
 
   return (
---
+++ src/services/CovidRestrictionsService.js
@@ -0,0 +1,81 @@
+const restrictionData = [
+   {
+        "date": "3/7/2021",
+        "positiveTests": 274,
+        "totalTested": 9045,
+        "dailyTestPositivity": 3.0,
+        "sevenDayAvg": 2.7
+    },
+    {
+        "date": "3/8/2021",
+        "positiveTests": 281,
+        "totalTested": 6631,
+        "dailyTestPositivity": 4.2,
+        "sevenDayAvg": 2.7
+    },
+    {
+        "date": "3/9/2021",
+        "positiveTests": 312,
+        "totalTested": 10244,
+        "dailyTestPositivity": 3.0,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/10/2021",
+        "positiveTests": 312,
+        "totalTested": 10244,
+        "dailyTestPositivity": 2.4,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/11/2021",
+        "positiveTests": 416,
+        "totalTested": 16873,
+        "dailyTestPositivity": 2.5,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/12/2021",
+        "positiveTests": 375,
+        "totalTested": 13794,
+        "dailyTestPositivity": 2.7,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/13/2021",
+        "positiveTests": 355,
+        "totalTested": 12661,
+        "dailyTestPositivity": 2.8,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/14/2021",
+        "positiveTests": 327,
+        "totalTested": 8712,
+        "dailyTestPositivity": 3.8,
+        "sevenDayAvg": 2.9
+    },
+    {
+        "date": "3/15/2021",
+        "positiveTests": 329,
+        "totalTested": 9022,
+        "dailyTestPositivity": 3.6,
+        "sevenDayAvg": 2.9
+    },
+    {
+        "date": "3/16/2021",
+        "positiveTests": 353,
+        "totalTested": 12629,
+        "dailyTestPositivity": 2.8,
+        "sevenDayAvg": 2.8
+    },
+    {
+        "date": "3/17/2021",
+        "positiveTests": 426,
+        "totalTested": 15605,
+        "dailyTestPositivity": 2.7,
+        "sevenDayAvg": 2.9
+    }
+];
+
+export default restrictionData;
